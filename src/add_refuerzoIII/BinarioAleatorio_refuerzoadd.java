3cd3e5d995fa76114c4a129254f4f00bcbd4664b
usuario <usuario@javi-System-Product-Name>
2017-04-07 11:30:02 +0200

    refuerzoIII terminado

--- src/add_refuerzoIII/BinarioAleatorio_refuerzoadd.java
+++ src/add_refuerzoIII/BinarioAleatorio_refuerzoadd.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package add_refuerzoIII;
 
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 /**
@@ -16,14 +10,16 @@ import java.util.Scanner;
  * @author usuario5
  */
 public class BinarioAleatorio_refuerzoadd {
+    
     static Scanner teclado = new Scanner(System.in);
+    static double min=50,max=0;
+    static int hmin=0, hmax=0;  
+   
     public static void main(String[]args) throws IOException{
         
        int [] horas = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
-       
-        
-        
-        
+            
+        //Crea el fichero y comprueba su existencia
         File fichero = new File("temperaturas.tmp");
         if(!fichero.exists()){
             fichero.createNewFile();
@@ -32,19 +28,65 @@ public class BinarioAleatorio_refuerzoadd {
             System.out.println("El archivo 'temperaturas.tmp' ya  ha sido creado anteriormente");
         }
         
-        DataOutputStream dataOS = new DataOutputStream(new FileOutputStream(fichero)); 
-        
-        for(int i=0;i<horas.length;i++){
-            dataOS.writeInt(horas[i]);
-            dataOS.writeDouble((double)Math.random()*(100-10)+10);
-            dataOS.writeDouble((double)Math.random()*(40-0)+0);
-        }        
+        //Escritura aleatoria del fichero
+        RandomAccessFile dataOS = new RandomAccessFile(fichero,"rw");
+        try{
+            int datapos=0;
+            dataOS.seek(datapos);
+            for(int i=0;i<horas.length;i++){
+                
+                dataOS.writeInt(horas[i]);
+                dataOS.writeDouble((int) (Math.random() * (100-10) + 10));
+                dataOS.writeDouble((int) (Math.random() * (40-0) + 0));
+                
+                datapos=datapos+20;
+                dataOS.seek(datapos);
+            }
+        }catch(IOException ex){
+            System.err.println("Error -> "+ex.getMessage());
+        }
         dataOS.close();
         
+        //Lectura aleatoria del fichero
+        RandomAccessFile rfile = new RandomAccessFile(fichero,"r");
+        try{
+        int pos=0;
+        rfile.seek(pos);
+        
+        
+            while(true){
+                    int h =rfile.readInt();
+                    double p= rfile.readDouble();
+                    double t = rfile.readDouble();
+                    System.out.println("Hora: "+h+" ,Presion: "+p+" ,Temperatura: "+t);
 
+
+                    if(t<min){
+                        min=t;
+                        hmin=h;
+                    }
+
+                    if(t>max){
+                        max=t;
+                        hmax=h;
+                    }
+                 pos=pos+20;
+                 rfile.seek(pos);
+            }
+            
+            
+            
+        }catch(IOException e){
+            System.err.println("Error -> "+e.getMessage());
+        }  
         
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("La temperatura minima fue de "+min+" grados a las "+hmin+" horas");
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("La temperatura maxima fue de "+max+" grados a las "+hmax+" horas");
+        System.out.println("--------------------------------------------------------------------");
         
-        
+        rfile.close();
     }
     
 }
