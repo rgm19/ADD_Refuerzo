4372a5d835d8eae9cd44190fc60a46f4db957c0f
ruben <ruben@BASE>
2017-06-01 17:13:01 +0200

    2.1

--- src/add_refuerzoVII/Hospital_neodatis.java
+++ src/add_refuerzoVII/Hospital_neodatis.java
@@ -42,7 +42,7 @@ public class Hospital_neodatis {
         pacientes.add(new Pacientes(6,"Julio","Abril","2015-02-05","52D","2017-04-15"));
         pacientes.add(new Pacientes(7,"Carmen","Peña","2016-02-05","49F","2017-04-15"));
         pacientes.add(new Pacientes(8,"Carmen","Peña","2015-02-05","49F","2017-04-15"));
-        pacientes.add(new Pacientes(9,"Angel","Rubi","2017-01-05","19E","2006-01-11"));
+        pacientes.add(new Pacientes(9,"Angel","Rubi","2006-01-05","19E","2006-01-15"));
         pacientes.add(new Pacientes(10,"Manuel","Lopez","2017-01-05","55M","2017-04-15"));
         pacientes.add(new Pacientes(11,"Manuel","Lopez","2017-03-05","55M","2017-04-15"));
         pacientes.add(new Pacientes(12,"Teresa","Toro","2017-02-05","54T","2017-04-15"));
@@ -94,7 +94,7 @@ private static void menu(){
                 break;
                 
             case 4:
-                
+                altaMas10Anios();
                 break;
                             
             case 5:
@@ -180,4 +180,20 @@ private static void menu(){
         BigInteger numPersona = odb.count((CriteriaQuery) query5);
         System.out.println("Numero de personas: " + numPersona);
     }
+//------------------------------------------------------------------------------
+    private static void altaMas10Anios() {
+         IQuery query = new CriteriaQuery(Pacientes.class, Where.and().add(Where.like("fechaAlta", "200%")));
+       Objects<Pacientes> object = odb.getObjects(query);
+       int i=0;
+       
+       while(object.hasNext()){
+           Pacientes p = object.next();
+           System.out.println((i++) + p.toString());
+           System.out.println();
+           
+       }
+       
+       if(i==0)System.out.println("Ningun paciente fue dado de alta hace mas de 10 años");
+    }
+//------------------------------------------------------------------------------    
 }
